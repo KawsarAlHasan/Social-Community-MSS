2d6eca1facba309a32a60d972977d60392f5937f
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-08-15 16:17:51 +0600

    creating info card

---
+++ src/components/InfoCard/InfoCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./InfoCard.css";
+import { UilPen } from "@iconscout/react-unicons";
+const InfoCard = () => {
+  return (
+    <div className="InfoCard">
+      <h4>Your Info</h4>
+      <UilPen></UilPen>
+    </div>
+  );
+};
+
+export default InfoCard;
---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ProfileLeft from "../../components/ProfileLeft/ProfileLeft";
+import "./Profile.css";
+
+const Profile = () => {
+  return (
+    <div className="Profile">
+      <ProfileLeft></ProfileLeft>
+    </div>
+  );
+};
+
+export default Profile;
