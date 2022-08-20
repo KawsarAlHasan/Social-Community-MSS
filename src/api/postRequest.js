35b993ae80180ab72b133948ff5d6e0799136020
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-08-21 00:40:52 +0600

    error solved

--- src/api/postRequest.js
+++ src/api/postRequest.js
@@ -1,5 +1,4 @@
 import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
-
-export const getTimelinePosts = (id) => API.get(`/posts/${id}/timeline`);
+export const getTimelinePosts = (id) => API.get(`/post/${id}/timeline`);
