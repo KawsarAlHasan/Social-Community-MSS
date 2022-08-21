acdfc5c2375efe2fead91c9bf8bc364ca297b97a
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-21 12:20:18 +0600

    Profile Card Setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import "./App.css";
-import { useSelector } from "react-redux/es/exports";
-import Auth from "./pages/Auth/Auth";
-import Home from "./pages/home/Home";
-import { Routes, Route, Navigate } from "react-router-dom";
+import './App.css'
+import { useSelector } from 'react-redux/es/exports'
+import Auth from './pages/Auth/Auth'
+import Home from './pages/home/Home'
+import Profile from './pages/Profile/Profile'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 function App() {
-  const user = useSelector((state) => state.authReducer.authData);
+  const user = useSelector((state) => state.authReducer.authData)
   return (
     <div className="App">
-      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
-      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <div className="blur" style={{ top: '-18%', right: '0' }}></div>
+      <div className="blur" style={{ top: '36%', left: '-8rem' }}></div>
       <Routes>
         <Route
           path="/"
@@ -23,9 +24,13 @@ function App() {
           path="/auth"
           element={user ? <Navigate to="../home" /> : <Auth />}
         />
+        <Route
+          path="profile/:id"
+          element={user ? <Profile /> : <Navigate to="../auth" />}
+        />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
