a45efc45c05378c03bc92ada8e16c50b11d0ba6f
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-08-21 13:12:03 +0600

    Log Out of info card

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,17 +1,26 @@
 const authReducer = (
   state = { authData: null, loading: false, error: false },
-  action
+  action,
 ) => {
   switch (action.type) {
-    case "AUTH_START":
-      return { ...state, loading: true, error: false };
-    case "AUTH_SUCCESS":
-      localStorage.setItem("profile", JSON.stringify({ ...action?.data }));
-      return { ...state, authData: action.data, loading: false };
-    case "AUTH_FAIL":
-      return { ...state, loading: false, error: true };
+    case 'AUTH_START':
+      return { ...state, loading: true, error: false }
+    case 'AUTH_SUCCESS':
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }))
+      return { ...state, authData: action.data, loading: false }
+    case 'AUTH_FAIL':
+      return { ...state, loading: false, error: true }
+    case 'LOG_OUT':
+      localStorage.clear()
+      return {
+        ...state,
+        authData: null,
+        loading: false,
+        error: false,
+        updateLoading: false,
+      }
     default:
-      return state;
+      return state
   }
-};
-export default authReducer;
+}
+export default authReducer
